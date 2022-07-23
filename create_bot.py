8450732883caa03398407fdca7589572c68a1667
dimaermakoff <108678145+dimaermakoff@users.noreply.github.com>
2022-07-23 14:54:45 +0300

    Delete create_bot.py

--- create_bot.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot
-from aiogram.dispatcher import Dispatcher
-import os
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-TOKEN = '5542785678:AAFx4K8qVleVYLRuKBbOZqGwGLQUxSZiriw'
-storage=MemoryStorage()
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot, storage=storage)
